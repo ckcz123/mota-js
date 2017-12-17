4df095ffc5af39aed47a0cf2e69b2bade90d152c
ckcz123 <ckcz123@126.com>
2017-12-17 21:04:07 +0800

    更多的默认素材

--- libs/maps.js
+++ libs/maps.js
@@ -78,8 +78,10 @@ maps.prototype.getBlock = function (x, y, id) {
     if (id == 12) tmp.event = {'cls': 'animates', 'id': 'poisonNet', 'noPass': false, 'trigger': 'passNet'}; // 毒网
     if (id == 13) tmp.event = {'cls': 'animates', 'id': 'weakNet', 'noPass': false, 'trigger': 'passNet'}; // 衰网
     if (id == 14) tmp.event = {'cls': 'animates', 'id': 'curseNet', 'noPass': false, 'trigger': 'passNet'}; // 咒网
+    if (id == 15) tmp.event = {'cls': 'animates', 'id': 'water', 'noPass': true}; // 水
 
-    if (id == 20) tmp.event = {'cls': 'autotile', 'id': 'autotile', 'noPass': true};
+    // autotile: 20
+    if (id == 20) tmp.event = {'cls': 'autotile', 'id': 'autotile', 'noPass': true}; // autotile
 
     // 21-80 物品
     if (id == 21) tmp.event = {'cls': 'items', 'id': 'yellowKey'}; // 黄钥匙

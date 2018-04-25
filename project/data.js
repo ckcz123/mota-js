083d6e6ee2490ca9ac70ea0e6f267ccc2ce14266
oc <ckcz123@126.com>
2018-04-25 15:27:12 +0800

    setHeroIcon

--- project/data.js
+++ project/data.js
@@ -5,7 +5,7 @@ data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 			"sample0", "sample1", "sample2", "MT0"
 		],
 		"images" : [
-			"bg.jpg", "hero0.png"
+			"bg.jpg"
 		],
 		"animates" : [ 
 			"hand", "sword", "zone",

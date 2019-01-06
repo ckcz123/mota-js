1c196d05f9bafa3f8c456bc09c903155925fa2b4
ckcz123 <ckcz123@126.com>
2019-01-06 14:18:10 +0800

    __VERSION__

--- project/functions.js
+++ project/functions.js
@@ -1191,7 +1191,7 @@ var functions_d6ad677b_427a_4623_b50f_a445a3b0ef8a =
 	core.setTextAlign('ui', 'left');
 	var globalFont = (core.status.globalAttribute||core.initStatus.globalAttribute).font;
 	core.fillText('ui', "HTML5 魔塔样板", text_start, top+35, "#FFD700", "bold 22px "+globalFont);
-	core.fillText('ui', "版本： "+core.firstData.version, text_start, top + 80, "#FFFFFF", "bold 17px "+globalFont);
+	core.fillText('ui', "版本： "+main.__VERSION__, text_start, top + 80, "#FFFFFF", "bold 17px "+globalFont);
 	core.fillText('ui', "作者： 艾之葵", text_start, top + 112);
 	core.fillText('ui', 'HTML5魔塔交流群：539113091', text_start, top+112+32);
 	// TODO: 写自己的“关于”页面，每次增加32像素即可

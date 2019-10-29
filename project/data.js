9848b9bfe55790932c26a22abd524978ae38e91d
oc <ckcz123@126.com>
2019-10-29 20:27:12 +0800

    v2.6.4

--- project/data.js
+++ project/data.js
@@ -78,7 +78,7 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 	"firstData": {
 		"title": "魔塔样板",
 		"name": "template",
-		"version": "Ver 2.6.3",
+		"version": "Ver 2.6.4",
 		"floorId": "sample0",
 		"hero": {
 			"name": "阳光",

a2beb2fffc56eaa4c8e3657e2c08b7f58f6e2f9e
tocque <364004564@qq.com>
2019-11-01 23:17:08 +0800

    modify tablebuilding

--- _server/table/comment.js
+++ _server/table/comment.js
@@ -300,66 +300,74 @@ var comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 					"_data": {
 						"floorId": {
 							"_leaf": true,
-							"_type": "textarea",
-							"_range": "false",
+							"_type": "em",
+							"_name": "楼层ID",
 							"_data": "文件名和floorId需要保持完全一致 \n楼层唯一标识符仅能由字母、数字、下划线组成，且不能由数字开头 \n推荐用法：第20层就用MT20，第38层就用MT38，地下6层就用MT_6（用下划线代替负号），隐藏3层用MT3h（h表示隐藏），等等 \n楼层唯一标识符，需要和名字完全一致 \n这里不能更改floorId,请通过另存为来实现"
 						},
 						"title": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
+							"_name": "楼层名称",
 							"_data": "楼层中文名，将在切换楼层和浏览地图时显示"
 						},
 						"name": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
+							"_name": "层数",
 							"_data": "显示在状态栏中的层数"
 						},
 						"width": {
 							"_leaf": true,
-							"_type": "textarea",
-							"_range": "false",
+							"_type": "em",
+							"_name": "地图宽度",
 							"_data": "地图x方向大小,这里不能更改,仅能在新建地图时设置,null视为13"
 						},
 						"height": {
 							"_leaf": true,
-							"_type": "textarea",
-							"_range": "false",
+							"_type": "em",
+							"_name": "地图高度",
 							"_data": "地图y方向大小,这里不能更改,仅能在新建地图时设置,null视为13"
 						},
 						"canFlyTo": {
 							"_leaf": true,
 							"_type": "checkbox",
 							"_bool": "bool",
+							"_name": "允许楼传",
 							"_data": "该楼能否被楼传器飞到（不能的话在该楼也不允许使用楼传器）"
 						},
 						"canUseQuickShop": {
 							"_leaf": true,
 							"_type": "checkbox",
 							"_bool": "bool",
+							"_name": "允许商店",
 							"_data": "该层是否允许使用快捷商店"
 						},
 						"cannotViewMap": {
 							"_leaf": true,
 							"_type": "checkbox",
 							"_bool": "bool",
+							"_name": "禁止浏览",
 							"_data": "该层是否不允许被浏览地图看到；如果勾上则浏览地图会跳过该层"
 						},
 						"cannotMoveDirectly": {
 							"_leaf": true,
 							"_type": "checkbox",
 							"_bool": "bool",
+							"_name": "禁止瞬移",
 							"_data": "该层是否不允许瞬间移动；如果勾上则不可在此层进行瞬移"
 						},
 						"firstArrive": {
 							"_leaf": true,
 							"_type": "event",
 							"_event": "firstArrive",
+							"_name": "初次到达事件",
 							"_data": "第一次到该楼层触发的事件，可以双击进入事件编辑器。"
 						},
 						"eachArrive": {
 							"_leaf": true,
 							"_type": "event",
 							"_event": "eachArrive",
+							"_name": "每次到达事件",
 							"_data": "每次到该楼层触发的事件，可以双击进入事件编辑器；该事件会在firstArrive执行后再执行。"
 						},
 						"parallelDo": {
@@ -367,23 +375,27 @@ var comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 							"_type": "textarea",
 							"_string": true,
 							"_lint": true,
+							"_name": "并行事件",
 							"_data": "在该层楼时执行的并行事件处理。\n可以在这里写上任意需要自动执行的脚本，比如打怪自动开门等。\n详见文档-事件-并行事件处理。"
 						},
 						"upFloor": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
+							"_name": "上楼点",
 							"_range": "thiseval==null||((thiseval instanceof Array) && thiseval.length==2)",
 							"_data": "该层上楼点，如[2,3]。\n如果此项不为null，则楼层转换时的stair:upFloor，以及楼传器的落点会被替换成该点而不是该层的上楼梯。"
 						},
 						"downFloor": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
+							"_name": "下楼点",
 							"_range": "thiseval==null||((thiseval instanceof Array) && thiseval.length==2)",
 							"_data": "该层下楼点，如[2,3]。\n如果此项不为null，则楼层转换时的stair:downFloor，以及楼传器的落点会被替换成该点而不是该层的下楼梯。"
 						},
 						"defaultGround": {
 							"_leaf": true,
 							"_type": "select",
+							"_name": "地面图块",
 							"_select": {
 								"values": Object.keys(editor.core.icons.icons.terrains)
 							},
@@ -392,16 +404,19 @@ var comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 						"images": {
 							"_leaf": true,
 							"_type": "textarea",
+							"_name": "背景/前景图",
 							"_data": "背景/前景图；你可以选择若干张图片来作为背景/前景素材。详细用法请参见文档“自定义素材”中的说明。"
 						},
 						"color": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
+							"_name": "画面色调",
 							"_data": "该层的默认画面色调。本项可不写（代表无色调），如果写需要是一个RGBA数组如[255,0,0,0.3]"
 						},
 						"weather": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
+							"_name": "天气",
 							"_data": "该层的默认天气。本项可忽略表示晴天，如果写则第一项为\"rain\"，\"snow\"或\"fog\"代表雨雪雾，第二项为1-10之间的数代表强度。\n如[\"rain\", 8]代表8级雨天。"
 						},
 						"bgm": {
@@ -410,11 +425,13 @@ var comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 							"_select": {
 								"values": [null].concat(Object.keys(editor.core.material.bgms))
 							},
+							"_name": "BGM",
 							"_data": "到达该层后默认播放的BGM。本项可忽略，或者为一个定义过的背景音乐如\"bgm.mp3\"。"
 						},
 						"item_ratio": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
+							"_name": "宝石/血瓶倍率",
 							"_range": "(thiseval==~~thiseval && thiseval>=0)||thiseval==null",
 							"_data": "每一层的宝石/血瓶效果，即获得宝石和血瓶时框内\"ratio\"的值。"
 						},
@@ -422,6 +439,7 @@ var comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 							"_leaf": true,
 							"_type": "checkbox",
 							"_bool": "bool",
+							"_name": "地下层",
 							"_data": "是否是地下层；如果该项为true则同层传送将传送至上楼梯"
 						}
 					}
@@ -433,36 +451,42 @@ var comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 							"_leaf": true,
 							"_type": "event",
 							"_event": "event",
+							"_name": "可能事件列表",
 							"_data": "该点的可能事件列表，可以双击进入事件编辑器。"
 						},
 						"changeFloor": {
 							"_leaf": true,
 							"_type": "event",
 							"_event": "changeFloor",
+							"_name": "楼层转换事件",
 							"_data": "该点楼层转换事件；该事件不能和上面的events同时出现，否则会被覆盖"
 						},
 						"afterBattle": {
 							"_leaf": true,
 							"_type": "event",
 							"_event": "afterBattle",
+							"_name": "战后事件",
 							"_data": "该点战斗后可能触发的事件列表，可以双击进入事件编辑器。"
 						},
 						"afterGetItem": {
 							"_leaf": true,
 							"_type": "event",
 							"_event": "afterGetItem",
+							"_name": "获取道具后事件",
 							"_data": "该点获得道具后可能触发的事件列表，可以双击进入事件编辑器。"
 						},
 						"afterOpenDoor": {
 							"_leaf": true,
 							"_type": "event",
 							"_event": "afterOpenDoor",
+							"_name": "开门后事件",
 							"_data": "该点开完门后可能触发的事件列表，可以双击进入事件编辑器。"
 						},
 						"cannotMove": {
 							"_leaf": true,
 							"_type": "textarea",
 							"_range": "thiseval==null||(thiseval instanceof Array)",
+							"_name": "通行性设置",
 							"_data": "该点不可通行的方向 \n 可以在这里定义该点不能前往哪个方向，可以达到悬崖之类的效果\n例如 [\"up\", \"left\"] 代表该点不能往上和左走"
 						}
 					}
--- _server/table/data.comment.js
+++ _server/table/data.comment.js
@@ -148,21 +148,24 @@ var data_comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 			"_data": {
 				"title": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "text",
 					"_string": true,
-					"_data": "游戏名，将显示在标题页面以及切换楼层的界面中"
+					"_name": "游戏名称",
+					"_data": "游戏的名称，将显示在标题页面以及切换楼层的界面中"
 				},
 				"name": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "text",
 					"_string": true,
+					"_name": "游戏标识符",
 					"_range": "/^[a-zA-Z0-9_]{1,30}$/.test(thiseval)",
 					"_data": "游戏的唯一英文标识符。由英文、数字、下划线组成，不能超过30个字符。\n此项必须修改，其将直接影响到存档的定位！"
 				},
 				"version": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "text",
 					"_string": true,
+					"_name": "当前游戏版本",
 					"_data": "当前游戏版本；版本不一致的存档不能通用。"
 				},
 				"floorId": {
@@ -171,98 +174,117 @@ var data_comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 					"_select": {
 						"values": data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d.main.floorIds
 					},
+					"_name": "初始楼层",
 					"_range": "data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d.main.floorIds.indexOf(thiseval)!==-1",
 					"_data": "初始楼层的ID"
 				},
 				"hero": {
+					"_name": "初始属性",
 					"_type": "object",
 					"_data": {
 						"name": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "text",
 							"_string": true,
+							"_name": "勇士名",
 							"_data": "勇士名；可以改成喜欢的"
 						},
 						"lv": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始等级",
 							"_range": "thiseval==~~thiseval &&thiseval>0",
 							"_data": "初始等级，该项必须为正整数"
 						},
 						"hpmax": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始生命上限",
 							"_data": "初始生命上限，只有在enableHPMax开启时才有效"
 						},
 						"hp": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始生命值",
 							"_data": "初始生命值"
 						},
 						"manamax": {
 							"_leaf": true,
-							"_type": "textarea",
-							"_data": "魔力上限；此项非负才会生效（null或小于0都不会生效）"
+							"_type": "number",
+							"_name": "初始魔力上限",
+							"_data": "初始魔力上限；此项非负才会生效（null或小于0都不会生效）"
 						},
 						"mana": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始魔力值",
 							"_data": "初始魔力值，只在enableMana开启时才有效"
 						},
 						"atk": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始攻击",
 							"_data": "初始攻击"
 						},
 						"def": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始防御",
 							"_data": "初始防御"
 						},
 						"mdef": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始魔防",
 							"_data": "初始魔防"
 						},
 						"money": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始金币",
 							"_data": "初始金币"
 						},
 						"experience": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_type": "number",
+							"_name": "初始经验",
 							"_data": "初始经验"
 						},
 						"equipment": {
 							"_leaf": true,
 							"_type": "textarea",
+							"_name": "初始装备",
 							"_range": "thiseval instanceof Array",
 							"_data": "初始装上的装备，此处建议请直接留空数组"
 						},
 						"items": {
+							"_name": "初始道具",
 							"_type": "object",
 							"_data": {
 								"keys": {
 									"_leaf": true,
+									"_name": "初始钥匙",
 									"_type": "textarea",
 									"_range": "thiseval instanceof Object && !(thiseval instanceof Array)",
 									"_data": "初始三种钥匙个数"
 								},
 								"constants": {
 									"_leaf": true,
+									"_name": "初始永久道具",
 									"_type": "textarea",
 									"_range": "thiseval instanceof Object && !(thiseval instanceof Array)",
 									"_data": "初始永久道具个数，例如初始送手册可以写 {\"book\": 1}"
 								},
 								"tools": {
 									"_leaf": true,
+									"_name": "初始消耗道具",
 									"_type": "textarea",
 									"_range": "thiseval instanceof Object && !(thiseval instanceof Array)",
 									"_data": "初始消耗道具个数，例如初始有两破可以写 {\"pickaxe\": 2}"
 								},
 								"equips": {
 									"_leaf": true,
+									"_name": "初始装备",
 									"_type": "textarea",
 									"_range": "thiseval instanceof Object && !(thiseval instanceof Array)",
 									"_data": "初始装备个数，例如初始送铁剑可以写 {\"sword1\": 1}"
@@ -270,10 +292,12 @@ var data_comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 							}
 						},
 						"loc": {
+							"_name": "初始位置",
 							"_type": "object",
 							"_data": {
 								"direction": {
 									"_leaf": true,
+									"_name": "勇士初始方向",
 									"_type": "select",
 									"_data": "勇士初始方向",
 									"_select": {
@@ -287,11 +311,13 @@ var data_comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 								},
 								"x": {
 									"_leaf": true,
+									"_name": "勇士初始x坐标",
 									"_type": "textarea",
 									"_data": "勇士初始x坐标"
 								},
 								"y": {
 									"_leaf": true,
+									"_name": "勇士初始y坐标",
 									"_type": "textarea",
 									"_data": "勇士初始y坐标"
 								}
@@ -305,7 +331,8 @@ var data_comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 						},
 						"steps": {
 							"_leaf": true,
-							"_type": "textarea",
+							"_name": "初始步数",
+							"_type": "number",
 							"_data": "行走步数统计"
 						}
 					}
@@ -345,87 +372,104 @@ var data_comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 			"_data": {
 				"lavaDamage": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "血网伤害",
 					"_data": "经过血网受到的伤害"
 				},
 				"poisonDamage": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "中毒伤害",
 					"_data": "中毒后每步受到的伤害"
 				},
 				"weakValue": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "衰弱效果",
 					"_data": "衰弱状态下攻防减少的数值\n如果此项不小于1，则作为实际下降的数值（比如10就是攻防各下降10）\n如果在0到1之间则为下降的比例（比如0.3就是下降30%的攻防）"
 				},
 				"redJewel": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "红宝石效果",
 					"_data": "红宝石加攻击的数值"
 				},
 				"blueJewel": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "蓝宝石效果",
 					"_data": "蓝宝石加防御的数值"
 				},
 				"greenJewel": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "绿宝石效果",
 					"_data": "绿宝石加魔防的数值"
 				},
 				"redPotion": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "红血瓶效果",
 					"_data": "红血瓶加血数值"
 				},
 				"bluePotion": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "蓝血瓶效果",
 					"_data": "蓝血瓶加血数值"
 				},
 				"yellowPotion": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "黄血瓶效果",
 					"_data": "黄血瓶加血数值"
 				},
 				"greenPotion": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "绿血瓶效果",
 					"_data": "绿血瓶加血数值"
 				},
 				"breakArmor": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "破甲比例",
 					"_data": "破甲的比例（战斗前，怪物附加角色防御的x倍作为伤害）"
 				},
 				"counterAttack": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "反击比例",
 					"_data": "反击的比例（战斗时，怪物每回合附加角色攻击的x倍作为伤害，无视角色防御）"
 				},
 				"purify": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "净化比例",
 					"_data": "净化的比例（战斗前，怪物附加勇士魔防的x倍作为伤害）"
 				},
 				"hatred": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "仇恨效果",
 					"_data": "仇恨属性中，每杀死一个怪物获得的仇恨值"
 				},
 				"moveSpeed": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "行走速度",
 					"_data": "行走速度，即勇士每走一格的时间，一般100比较合适"
 				},
 				"animateSpeed": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "动画速度",
 					"_data": "全局动画时间，即怪物振动频率，一般300比较合适"
 				},
 				"floorChangeTime": {
 					"_leaf": true,
-					"_type": "textarea",
+					"_type": "number",
+					"_name": "楼层切换时间",
 					"_data": "默认楼层切换时间"
 				}
 			}
@@ -437,282 +481,329 @@ var data_comment_c456ea59_6018_45ef_8bcc_211a24c627dc = {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "状态栏显示楼层",
 					"_data": "是否在状态栏显示当前楼层"
 				},
 				"enableName": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "状态栏显示名字",
 					"_data": "是否在状态栏显示勇士名字"
 				},
 				"enableLv": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "状态栏显示等级",
 					"_data": "是否在状态栏显示当前等级"
 				},
 				"enableHPMax": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
-					"_data": "是否是否启用生命上限"
+					"_name": "启用生命上限",
+					"_data": "是否启用生命上限"
 				},
 				"enableMana": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "启用魔力值",
 					"_data": "是否开启魔力值"
 				},
 				"enableMDef": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "启用魔防",
 					"_data": "是否在状态栏及战斗界面显示魔防（护盾）"
 				},
 				"enableMoney": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "启用金币",
 					"_data": "是否在状态栏、怪物手册及战斗界面显示金币"
 				},
 				"enableExperience": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "启用经验",
 					"_data": "是否在状态栏、怪物手册及战斗界面显示经验"
 				},
 				"enableLevelUp": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "允许升级",
 					"_data": "是否允许等级提升（进阶）；如果上面enableExperience为false，则此项恒视为false"
 				},
 				"levelUpLeftMode": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "扣除模式",
 					"_data": "进阶使用扣除模式，即在状态栏显示距离下个等级所需要的经验值；只有enableExperience和enableLevelUp均开启时才有效。"
 				},
 				"enableKeys": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "显示钥匙",
 					"_data": "是否在状态栏显示三色钥匙数量"
 				},
 				"enablePZF": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "显示破炸飞",
 					"_data": "是否在状态栏显示破炸飞数量"
 				},
 				"enableDebuff": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "显示毒衰咒",
 					"_data": "是否在状态栏显示毒衰咒"
 				},
 				"enableSkill": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "启用技能栏",
 					"_data": "是否启用技能栏"
 				},
 				"flyNearStair": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "楼梯旁传送",
 					"_data": "传送器是否需要在楼梯边使用；如果flyRecordPosition开启，则此项对箭头也有效。"
 				},
 				"flyRecordPosition": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "平面传送",
 					"_data": "传送器平面塔模式；此模式下楼层传送器将飞到上次离开该楼层的位置。"
 				},
 				"pickaxeFourDirections": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "四方向破墙",
 					"_data": "使用破墙镐是否四个方向都破坏；如果false则只破坏面前的墙壁"
 				},
 				"bombFourDirections": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "四方向炸弹",
 					"_data": "使用炸弹是否四个方向都会炸；如果false则只炸面前的怪物（即和圣锤等价）"
 				},
 				"snowFourDirections": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "四方向冰冻",
 					"_data": "使用冰冻徽章是否四个方向都会消除熔岩；如果false则只消除面前的熔岩"
 				},
 				"bigKeyIsBox": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "钥匙盒",
 					"_data": "如果此项为true，则视为钥匙盒，红黄蓝钥匙+1；若为false，则视为大黄门钥匙"
 				},
 				"steelDoorWithoutKey": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "铁门直接开启",
 					"_data": "铁门是否不需要钥匙开启。如果此项为true，则无需钥匙也可以开铁门。"
 				},
 				"itemFirstText": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "首次获得提示",
 					"_data": "首次获得道具是否提示"
 				},
 				"equipment": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "装备系统",
 					"_data": "剑和盾是否作为装备。如果此项为true，则作为装备，需要在装备栏使用，否则将直接加属性。"
 				},
 				"equipboxButton": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "装备按钮",
 					"_data": "状态栏的装备按钮。若此项为true则将状态栏中的楼层转换器按钮换为装备栏按钮"
 				},
 				"iconInEquipbox": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "装备效果图标化",
 					"_data": "在装备栏中的属性变化，是否绘制图标；如果此项开启，则会绘制图标而不是文字"
 				},
 				"enableAddPoint": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "开启加点",
 					"_data": "是否支持加点"
 				},
 				"enableNegativeDamage": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "开启负伤害",
 					"_data": "是否支持负伤害（回血）"
 				},
 				"hatredDecrease": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "仇恨战后减半",
 					"_data": "是否在和仇恨怪战斗后减一半的仇恨值，此项为false则和仇恨怪不会扣减仇恨值。"
 				},
 				"betweenAttackCeil": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "夹击向上取整",
 					"_data": "夹击上整还是下整。如果此项为true则夹击伤害值向上取整，为false则为向下取整"
 				},
 				"betweenAttackMax": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "夹击伤害上限",
 					"_data": "夹击伤害是否不超过怪物伤害值。"
 				},
 				"useLoop": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "循环计算临界",
 					"_data": "是否循环计算临界；如果此项为true则使用循环法（而不是回合数计算法）来算临界\n从V2.5.3开始，对于大数据的循环法将改为使用二分法进行计算"
 				},
 				"startUsingCanvas": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "开始菜单canvas化",
 					"_data": "是否开始菜单canvas化；如果此项为true，则将使用canvas来绘制开始菜单"
 				},
 				"startDirectly": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "不显示难度选择",
 					"_data": "点击“开始游戏”后是否立刻开始游戏而不显示难度选择界面"
 				},
 				"statusCanvas": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "状态栏canvas化",
 					"_data": "是否状态栏canvas化，即手动自定义绘制状态栏。\n如果此项开启，则可在脚本编辑的drawStatusBar中自定义绘制菜单栏。"
 				},
 				"statusCanvasRowsOnMobile": {
 					"_leaf": true,
 					"_type": "textarea",
 					"_range": "thiseval==null || (thiseval>0 && thiseval<=4)",
+					"_name": "竖屏状态栏行数",
 					"_data": "竖屏模式下，顶端状态栏canvas化后的行数。\n此项将决定竖屏的状态栏高度，如果设置则不小于1且不大于4。\n仅在statusCanvas开启时才有效"
 				},
 				"displayEnemyDamage": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "地图怪物显伤",
 					"_data": "是否地图怪物显伤；用户可以手动在菜单栏中开关"
 				},
 				"displayCritical": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "地图临界显示",
 					"_data": "是否地图显示临界；用户可以手动在菜单栏中开关"
 				},
 				"displayExtraDamage": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "地图高级显伤",
 					"_data": "是否地图高级显伤（领域、夹击等）；用户可以手动在菜单栏中开关"
 				},
 				"enableGentleClick": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "允许轻触",
 					"_data": "是否允许轻触（获得面前物品）"
 				},
 				"potionWhileRouting": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "寻路算法经过血瓶",
 					"_data": "寻路算法是否经过血瓶；如果该项为false，则寻路算法会自动尽量绕过血瓶"
 				},
 				"ignoreChangeFloor": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "楼梯穿透",
 					"_data": "经过楼梯、传送门时是否能“穿透”。\n穿透的意思是，自动寻路得到的的路径中间经过了楼梯，行走时是否触发楼层转换事件"
 				},
 				"canGoDeadZone": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "将死领域",
 					"_data": "是否允许走到将死的领域上。如果此项为true，则可以走到将死的领域上"
 				},
 				"enableMoveDirectly": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "允许瞬移",
 					"_data": "是否允许瞬间移动"
 				},
 				"enableDisabledShop": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "预览商店",
 					"_data": "是否允许查看未开启状态的快捷商店内容；如果此项为真，则对于未开启状态的商店允许查看其内容（但不能购买）"
 				},
 				"disableShopOnDamage": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "受限快捷商店",
 					"_data": "是否在经过领域/夹击/路障等伤害后禁用快捷商店。"
 				},
 				"blurFg": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "前景虚化",
 					"_data": "是否虚化前景层；如果此项开启，则在游戏中事件层有东西（如宝石等）时虚化前景层。"
 				},
 				"checkConsole": {
 					"_leaf": true,
 					"_type": "checkbox",
 					"_bool": "bool",
+					"_name": "检查控制台",
 					"_data": "是否检查控制台的开启情况。"
 				}
 			}

cfecb91bbe7c58e69336cab621dc9b3613b39cf4
InstantMeteor <chuan_@outlook.com>
2019-11-30 11:35:17 +0800

    Update ui.js

--- libs/ui.js
+++ libs/ui.js
@@ -1496,9 +1496,10 @@ ui.prototype.drawSwitchs = function() {
     var choices = [
         "背景音乐： "+(core.musicStatus.bgmStatus ? "[ON]" : "[OFF]"),
         "背景音效： "+(core.musicStatus.soundStatus ? "[ON]" : "[OFF]"),
-        //显示为 1~10 十挡
-        " <   " + "音量："+parseInt(core.musicStatus.userVolume* 10) + "   > ",
-        "行走速度： "+parseInt(core.values.moveSpeed),
+        //显示为 0~10 十挡
+        " <     "+"音量："+parseInt(parseFloat(Math.sqrt(core.musicStatus.userVolume).toFixed(1)) * 10)+"     > ",
+        //显示为 1~15 十五挡
+        " < "+"行走速度：" + parseInt((210 - core.values.moveSpeed) / 10) +" > ",
         "怪物显伤： "+(core.flags.displayEnemyDamage ? "[ON]" : "[OFF]"),
         "临界显伤： "+(core.flags.displayCritical ? "[ON]" : "[OFF]"),
         "领域显伤： "+(core.flags.displayExtraDamage ? "[ON]" : "[OFF]"),

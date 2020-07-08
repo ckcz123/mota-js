bd1c664fb395ef44bae72f7baaf553b6e8e747f3
ckcz123 <ckcz123@126.com>
2020-07-08 12:00:40 +0800

    Fix replay stackoverflow

--- libs/control.js
+++ libs/control.js
@@ -1568,9 +1568,7 @@ control.prototype.__replay_getTimeout = function () {
 
 control.prototype._replayAction_move = function (action) {
     if (["up","down","left","right"].indexOf(action)<0) return false;
-    core.moveHero(action, function () {
-        setTimeout(core.replay);
-    });
+    core.moveHero(action, core.replay);
     return true;
 }
 

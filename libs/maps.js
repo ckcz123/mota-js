2167a6eabe20e9f9c51f780cdccb83b7591fc328
ckcz123 <ckcz123@126.com>
2020-07-08 13:58:26 +0800

    Fix compressMap bug

--- libs/maps.js
+++ libs/maps.js
@@ -358,10 +358,8 @@ maps.prototype.saveMap = function (floorId) {
 
     var map = maps[floorId];
     var thisFloor = this._compressFloorData(map, core.floors[floorId]);
-    if (map.blocks) {
-        var mapArr = this.compressMap(this._getMapArrayFromBlocks(map.blocks, map.width, map.height, true), floorId);
-        if (mapArr != null) thisFloor.map = mapArr;
-    }
+    var mapArr = this.compressMap(map.blocks ? this._getMapArrayFromBlocks(map.blocks, map.width, map.height, true) : map.map, floorId);
+    if (mapArr != null) thisFloor.map = mapArr;
     return thisFloor;
 }
 

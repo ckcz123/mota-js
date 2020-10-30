a1c1e4672c98858518b01806d07328a02f1c908d
ckcz123 <ckcz123@126.com>
2020-10-30 21:58:05 +0800

    状态栏自绘高清UI

--- project/data.js
+++ project/data.js
@@ -472,7 +472,7 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 		"betweenAttackMax": false,
 		"useLoop": false,
 		"startUsingCanvas": false,
-		"statusCanvas": false,
+		"statusCanvas": true,
 		"displayEnemyDamage": true,
 		"displayCritical": true,
 		"displayExtraDamage": true,

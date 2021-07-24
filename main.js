65e11ab0c48acd37fb7671be60469134589a9373
ckcz123 <ckcz123@126.com>
2021-07-24 22:20:19 +0800

    V2.8

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function main() {
 
     //------------------------ 用户修改内容 ------------------------//
 
-    this.version = "2.7.3.1"; // 游戏版本号；如果更改了游戏内容建议修改此version以免造成缓存问题。
+    this.version = "2.8"; // 游戏版本号；如果更改了游戏内容建议修改此version以免造成缓存问题。
 
     this.useCompress = false; // 是否使用压缩文件
     // 当你即将发布你的塔时，请使用“JS代码压缩工具”将所有js代码进行压缩，然后将这里的useCompress改为true。
@@ -186,8 +186,8 @@ function main() {
     this.floors = {}
     this.canvas = {};
 
-    this.__VERSION__ = "2.7.3.1";
-    this.__VERSION_CODE__ = 273;
+    this.__VERSION__ = "2.8";
+    this.__VERSION_CODE__ = 387;
 }
 
 main.prototype.init = function (mode, callback) {

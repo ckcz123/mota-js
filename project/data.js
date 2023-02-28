d6f4ceed14fe82bb88bb2c7a6a7b5f2c32f38009
unanmed <1319491857@qq.com>
2023-02-28 13:48:35 +0800

    修改版本号

--- project/data.js
+++ project/data.js
@@ -165,7 +165,7 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 	"firstData": {
 		"title": "魔塔样板",
 		"name": "template",
-		"version": "Ver 2.10.0",
+		"version": "Ver 2.10.3",
 		"floorId": "sample0",
 		"hero": {
 			"image": "hero.png",
